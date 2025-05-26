0eecc2ac45a57730276df9ba7c4eac7d797c82a2
Miraç Sucu <miracsucu4417@gmail.com>
2025-05-26 23:48:48 +0300

    Libft 6 fonksiyon eklendi

---
+++ Libft/libft.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: msucu <msucu@student.42kocaeli.com.tr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/26 23:11:17 by msucu             #+#    #+#             */
+/*   Updated: 2025/05/26 23:39:12 by msucu            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <stddef.h>
+
+size_t	ft_strlen(const	char *str);
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
+
+#endif
