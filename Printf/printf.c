ed933f0ab6335d0e5de50df5bb954b24243b01f5
Miraç Sucu <miracsucu4417@gmail.com>
2025-06-24 17:42:31 +0300

    Printf pdf eklendi

--- Printf/printf.c
+++ Printf/printf.c
@@ -6,13 +6,26 @@
 /*   By: msucu <msucu@student.42kocaeli.com.tr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/24 15:04:43 by msucu             #+#    #+#             */
-/*   Updated: 2025/06/24 15:12:38 by msucu            ###   ########.fr       */
+/*   Updated: 2025/06/24 17:40:11 by msucu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdarg.h>
 #include <unistd.h>
 
+static int	ft_find_type(char c)
+{
+	/* Türler:
+	0: char
+	1: string
+	2: pointer
+	3: d
+	*/
+	if (c == 'c')
+		return (0);
+}
+(c == 'c' || c == 's' || c == 'p' || c == 'd' || c == 'i'
+	|| c == 'u' || c == 'x' || c == 'X' || c == '%')
 int ft_printf(const char *format, ...)
 {
 	va_list args;
